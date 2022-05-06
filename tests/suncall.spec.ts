c336291322ec2aa525bdaae12aa0e83f44fd9873
armariya <arm.armariya@gmail.com>
2022-05-06 18:16:26 +0700

    test: add deposit test without airdrop solUST in devnet

--- tests/suncall.spec.ts
+++ tests/suncall.spec.ts
@@ -13,6 +13,7 @@ describe("suncall", () => {
     token: new anchor.web3.PublicKey("8yazwmgc66uKrDBy3TZpNCgLa8qUDcuH8PZCz9jy6dzd"),
     underlyingMint: new anchor.web3.PublicKey("5fjG31cbSszE6FodW37UJnNzgVTyqg5WHWGCmL3ayAvA"),
     mint: new anchor.web3.PublicKey("6XyygxFmUeemaTvA9E9mhH9FvgpynZqARVyG3gUdCMt7"),
+    program: new anchor.web3.PublicKey("YiiTopEnX2vyoWdXuG45ovDFYZars4XZ4w6td6RVTFm"),
   };
 
   const poolName = "solust_pool";
@@ -92,5 +93,55 @@ describe("suncall", () => {
       })
       .rpc();
     console.log("Deposit initialize", tx);
+  });
+
+  it("should deposit", async () => {
+    const [lottoPda] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        provider.wallet.publicKey.toBuffer(),
+        Buffer.from(poolName),
+        Buffer.from("lotto"),
+      ],
+      program.programId,
+    );
+    const [lottoAuthorityPda] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        lottoPda.toBuffer(),
+        Buffer.from("authority"),
+      ],
+      program.programId,
+    );
+    const lottoYiUnderlyingAta = await anchor.utils.token.associatedAddress({
+      mint: yi.underlyingMint,
+      owner: lottoAuthorityPda, 
+    });
+    const lottoYiAta = await anchor.utils.token.associatedAddress({
+      mint: yi.mint,
+      owner: lottoAuthorityPda,
+    });
+    const userYiUnderlyingAta = await anchor.utils.token.associatedAddress({
+      mint: yi.underlyingMint,
+      owner: provider.wallet.publicKey,
+    });
+    const yiUnderlyingAta = await anchor.utils.token.associatedAddress({
+      mint: yi.underlyingMint,
+      owner: yi.token,
+    });
+
+    const tx = await program.methods.deposit(new anchor.BN(10^6))
+      .accounts({
+        lotto: lottoPda,
+        yiUnderlyingMint: yi.underlyingMint,
+        yiMint: yi.mint,
+        yiToken: yi.token,
+        lottoYiUnderlyingAta,
+        lottoYiAta,
+        userYiUnderlyingAta,
+        yiUnderlyingAta,
+        user: provider.wallet.publicKey,
+        yiProgram: yi.program,
+      })
+      .rpc();
+    console.log("Deposit", tx);
   });
 });
